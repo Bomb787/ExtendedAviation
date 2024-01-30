e93509aed47b08403a25b7746e67d72a64c70ee9
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2024-01-29 19:05:21 -0500

    🔨 Starting work on the C172

---
+++ src/main/java/io/github/bomb787/extended_aviation/entities/C172Entity.java
@@ -0,0 +1,49 @@
+package io.github.bomb787.extended_aviation.entities;
+
+import immersive_aircraft.entity.AircraftEntity;
+import immersive_aircraft.entity.misc.AircraftProperties;
+import immersive_aircraft.entity.misc.VehicleInventoryDescription;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class C172Entity extends CustomAirplaneEntity {
+	
+	protected final AircraftProperties properties = new AircraftProperties(this)
+            .setYawSpeed(5.0f)
+            .setPitchSpeed(4.0f)
+            .setEngineSpeed(0.05f)
+            .setGlideFactor(0.05f)
+            .setDriftDrag(0.01f)
+            .setLift(0.15f)
+            .setRollFactor(45.0f)
+            .setGroundPitch(4.0f)
+            .setWindSensitivity(0.025f)
+            .setMass(1.0f);
+	
+	private static final VehicleInventoryDescription inventoryDescription = new VehicleInventoryDescription()
+			.addSlot(VehicleInventoryDescription.SlotType.BOILER, 8 + 9, 8 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.BOOSTER, 8 + 9, 8 + 48)
+	        .addSlot(VehicleInventoryDescription.SlotType.WEAPON, 8 + 18 * 2 + 6, 8 + 6)
+	        .addSlot(VehicleInventoryDescription.SlotType.BANNER, 8 + 18 * 2 + 28, 8 + 6)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22 * 2)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22 * 2)
+	        .addSlots(VehicleInventoryDescription.SlotType.INVENTORY, 8 + 18 * 5, 8, 4, 4)
+	        .build();
+
+	public C172Entity(EntityType<? extends AircraftEntity> entityType, Level world) {
+		super(entityType, world);
+	}
+	
+	@Override
+	public VehicleInventoryDescription getInventoryDescription() {
+		return inventoryDescription;
+	}
+
+	@Override
+	public AircraftProperties getProperties() {
+		return properties;
+	}
+
+}
--- src/main/java/io/github/bomb787/extended_aviation/entities/TestPlaneEntity.java
+++ src/main/java/io/github/bomb787/extended_aviation/entities/TestPlaneEntity.java
@@ -37,17 +37,17 @@ public class TestPlaneEntity extends CustomAirplaneEntity {
         return properties;
     }
 	
-	 private static final VehicleInventoryDescription inventoryDescription = new VehicleInventoryDescription()
-			 .addSlot(VehicleInventoryDescription.SlotType.BOILER, 8 + 9, 8 + 22)
-	         .addSlot(VehicleInventoryDescription.SlotType.BOOSTER, 8 + 9, 8 + 48)
-	         .addSlot(VehicleInventoryDescription.SlotType.WEAPON, 8 + 18 * 2 + 6, 8 + 6)
-	         .addSlot(VehicleInventoryDescription.SlotType.BANNER, 8 + 18 * 2 + 28, 8 + 6)
-	         .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22)
-	         .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22)
-	         .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22 * 2)
-	         .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22 * 2)
-	         .addSlots(VehicleInventoryDescription.SlotType.INVENTORY, 8 + 18 * 5, 8, 4, 4)
-	         .build();
+	private static final VehicleInventoryDescription inventoryDescription = new VehicleInventoryDescription()
+			.addSlot(VehicleInventoryDescription.SlotType.BOILER, 8 + 9, 8 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.BOOSTER, 8 + 9, 8 + 48)
+	        .addSlot(VehicleInventoryDescription.SlotType.WEAPON, 8 + 18 * 2 + 6, 8 + 6)
+	        .addSlot(VehicleInventoryDescription.SlotType.BANNER, 8 + 18 * 2 + 28, 8 + 6)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 6, 8 + 6 + 22 * 2)
+	        .addSlot(VehicleInventoryDescription.SlotType.UPGRADE, 8 + 18 * 2 + 28, 8 + 6 + 22 * 2)
+	        .addSlots(VehicleInventoryDescription.SlotType.INVENTORY, 8 + 18 * 5, 8, 4, 4)
+	        .build();
 
 	@Override
 	public VehicleInventoryDescription getInventoryDescription() {
